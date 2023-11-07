a604c5186df6437f2796432c34908e23c86161c3
Miguel Leal <miguel11065@gmail.com>
2023-11-07 16:42:35 -0300

    criando a tag

---
+++ src/Tag/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { cores } from '../styles'
+
+export const TagContainer = styled.div`
+  background-color: ${cores.indianRed};
+  color: ${cores.antiqueWhite};
+  font-size: 12px;
+  font-weight: bold;
+  padding: 4px 6px;
+`
